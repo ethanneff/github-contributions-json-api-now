437933c2fd94071130bc781f6215647740813acf
Ethan Neff <ethan.neff@eneff.com>
2019-12-22 15:18:07 -0800

    🤠

---
+++ pages/api/index.js
@@ -0,0 +1,31 @@
+import cheerio from "cheerio";
+import axios from "axios";
+
+export default async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+
+  const activity = {};
+  const username = req.query.id || req.query.username;
+  if (!username) {
+    res.statusCode = 400;
+    res.end(
+      JSON.stringify({
+        error: "missing github username",
+        example:
+          "https://github-contributions-json-api-9rbq4wpsw.now.sh/api/?username=gaearon"
+      })
+    );
+  }
+  const url = `https://github.com/${username}`;
+  const github = await axios.get(url);
+  const $ = cheerio.load(github.data);
+  $(".day").each((_, element) => {
+    const item = $(element);
+    const date = item.attr("data-date") || "";
+    const unix = new Date(date).valueOf();
+    const count = Number(item.attr("data-count"));
+    activity[unix] = count;
+  });
+  res.statusCode = 200;
+  res.end(JSON.stringify(activity));
+};
